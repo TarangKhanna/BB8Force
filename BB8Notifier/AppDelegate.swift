c92301c29d7ebeeecdc60677439acbb64f40eed4
Tarang Khanna <khanna17@purdue.edu>
2016-01-29 21:10:01 -0500

    initialize
    movement and connection

---
+++ BB8Notifier/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  bb8Notifier
+//
+//  Created by Tarang khanna on 1/29/16.
+//  Copyright © 2016 Tarang khanna. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
